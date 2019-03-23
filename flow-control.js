0f755aff367b17f6ad05130260847bde15d0c177
Michael OConnor <mikeoconnor20@gmail.com>
2019-03-23 23:58:00 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,40 @@
 function basicTeenager(age) {
-
+  if (age>12 && age<20) {
+    return "You are a teenager!"
+  }
 }
 
 function teenager(age) {
-
+  if (age>12 && age<20) {
+    return "You are a teenager!"
+  }
+  else {
+    return "You are not a teenager"
+  }
 }
 
 function ageChecker(age) {
-
+  if (age>12 && age<20) {
+    return "You are a teenager!"
+  }
+  else if (age<13) {
+    return "You are a kid"
+  }
+  else if (age>19) {
+    return "You are a grownup"
+  }
 }
 
 function ternaryTeenager(age) {
-
+return age>12 && age<20 ? "You are a teenager" : "You are not a teenager"
 }
 
-function switchAge(age) {
 
+function switchAge(age) {
+  switch(age) {
+    case age>12 && age<20:
+      return "You are a teenager"
+    default:
+      return "You are a teenager"
+  }
 }
