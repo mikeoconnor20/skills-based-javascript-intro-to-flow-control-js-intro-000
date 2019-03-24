3e28960628a04a2a4aeee477179e55c9662ee316
Michael OConnor <mikeoconnor20@gmail.com>
2019-03-24 00:01:06 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -32,7 +32,7 @@ return age>12 && age<20 ? "You are a teenager" : "You are not a teenager"
 
 function switchAge(age) {
   switch(age) {
-    case age>12 && age<20:
+    case (age>12 && age<20):
       return "You are a teenager"
       break;
   }
